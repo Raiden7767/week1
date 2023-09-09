9602208357f670d13900b61897de726ca4f40f32
KirillSemasko <ksemasko@tktk.ee>
2023-09-09 15:00:14 +0300

    week 1 code

---
+++ server.js
@@ -0,0 +1,16 @@
+//console.log("Hello, world!");
+
+//ctrl + s
+
+//ctrl + c
+
+let firstName = 'Harry';
+let lastName = "Potter";
+
+//console.log(firstName);
+
+//template literals
+
+let greeting = `Hello, ${firstName} ${lastName}`;
+
+console.log(greeting);
